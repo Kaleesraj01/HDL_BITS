39c5e7dac207776a42bac6e2711afbbe260bf865
Kaleesraj M <kalishraj01@gmail.com>
2025-11-09 22:54:52 +0530

    Create 2. 4bit shift register up and down .v

---
+++ Circuits /Building larger circuits /2. 4bit shift register up and down .v
@@ -0,0 +1,18 @@
+module top_module (
+    input clk,
+    input shift_ena,
+    input count_ena,
+    input data,
+    output [3:0] q);
+   
+    
+     always @(posedge clk) begin
+        if (shift_ena)
+            q <= {q[2:0], data};  // Shift left, MSB first
+        else if (count_ena)
+            q <= q - 1;           // Down counter
+    end
+
+    
+    
+endmodule
